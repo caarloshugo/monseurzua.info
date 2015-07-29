19c8febf2eabeb173edc5c180b2cdaade66e7fad
Carlos Hugo Gonzalez Castell <caarloshugo@gmail.com>
2015-07-29 14:11:06 -0500

    add temaplte

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<!--
+	Aerial by HTML5 UP
+	html5up.net | @n33co
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Monserrat Urzua - Ing. Telematica</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+	</head>
+	<body class="loading">
+		<div id="wrapper">
+			<div id="bg"></div>
+			<div id="overlay"></div>
+			<div id="main">
+
+				<!-- Header -->
+					<header id="header">
+						<h1>Monserrat Urzua Alvarez</h1>
+						<p>Maestría en Computación &nbsp;&bull;&nbsp; Administración de Redes (CCNA) &nbsp;&bull;&nbsp; Ingeniera en Telematica</p>
+						<nav>
+							<ul>
+								<li><a title="Twitter" href="http://twitter.com/mounse" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+								<li><a title="Curriculum" href="/curriculum-sarai-monserrat-urzua-alvarez.pdf" class="icon fa-folder"><span class="label">Curriculum</span></a></li>
+								<li><a title="LinkedIn" href="https://mx.linkedin.com/pub/monserrat-urzúa/25/34b/574" class="icon fa-linkedin"><span class="label">Linkedin</span></a></li>
+								<li><a title="Email" href="mailto:monse.urzua@gmail.com" class="icon fa-envelope-o"><span class="label">Email</span></a></li>
+							</ul>
+						</nav>
+					</header>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<span class="copyright">&copy; monseurzua.info 2015 - Design: <a href="http://html5up.net">HTML5 UP</a>.</span>
+					</footer>
+
+			</div>
+		</div>
+		<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+		<script>
+			window.onload = function() { document.body.className = ''; }
+			window.ontouchmove = function() { return false; }
+			window.onorientationchange = function() { document.body.scrollTop = 0; }
+		</script>
+	</body>
+</html>
